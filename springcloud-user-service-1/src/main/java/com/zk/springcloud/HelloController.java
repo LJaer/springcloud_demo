73ed78f9660fbbfe9a7fc717ae0a86c6bd0fefa4
zhangkai <zhangkai170308@credithc.com>
2018-11-01 14:15:24 +0800

    springcloud-user-service-1

---
+++ springcloud-user-service-1/src/main/java/com/zk/springcloud/HelloController.java
@@ -0,0 +1,17 @@
+package com.zk.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello World 9006";
+    }
+
+}
